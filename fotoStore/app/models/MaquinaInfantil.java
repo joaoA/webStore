b64bc90a098d82511d7a3791a005de67b9d0340f
Joao Amsellem <amsellem_joao@hotmail.com>
2012-12-10 15:15:47 +0000

    models

---
+++ fotoStore/app/models/MaquinaInfantil.java
@@ -0,0 +1,16 @@
+package models;
+
+public class MaquinaInfantil extends Maquina{
+
+	private String cor;
+
+	public MaquinaInfantil() {}
+
+	public String getCor() {
+		return cor;
+	}
+
+	public void setCor(String cor) {
+		this.cor = cor;
+	}
+}
---
+++ fotoStore/app/models/MaterialFoto.java
@@ -0,0 +1,28 @@
+package models;
+
+public class MaterialFoto {
+
+	public String titulo;
+	public String marca;
+	public String preco;
+
+	public String getTitulo() {
+		return titulo;
+	}
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+	public String getMarca() {
+		return marca;
+	}
+	public void setMarca(String marca) {
+		this.marca = marca;
+	}
+	public String getPreco() {
+		return preco;
+	}
+	public void setPreco(String preco) {
+		this.preco = preco;
+	}
+
+}
