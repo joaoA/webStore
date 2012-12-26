3b7e20fbeb0e8bc312966938340b4f64d4b3de0f
Joao Martins <jcgm227@gmail.com>
2012-12-26 21:20:19 +0000

    aspas a mais resolvido

--- fotoStore/app/controllers/Search.java
+++ fotoStore/app/controllers/Search.java
@@ -72,58 +72,58 @@ public class Search {
 			QuerySolution sol = results.nextSolution();
 			if(type.equals("Aventura")) {
 				matFoto = new MaquinaAventura();
-				((MaquinaAventura)matFoto).setZoom(sol.get("element").asResource().getProperty(props.zoom).asTriple().getMatchObject().toString());
-				((MaquinaAventura)matFoto).setAbertura(sol.get("element").asResource().getProperty(props.abertLente).asTriple().getMatchObject().toString());
-				((MaquinaAventura)matFoto).setIso(sol.get("element").asResource().getProperty(props.iso).asTriple().getMatchObject().toString());
-				((MaquinaAventura)matFoto).setVelocidade(sol.get("element").asResource().getProperty(props.velo).asTriple().getMatchObject().toString());
+				((MaquinaAventura)matFoto).setZoom(trimQuote(sol.get("element").asResource().getProperty(props.zoom).asTriple().getMatchObject().toString()));
+				((MaquinaAventura)matFoto).setAbertura(trimQuote(sol.get("element").asResource().getProperty(props.abertLente).asTriple().getMatchObject().toString()));
+				((MaquinaAventura)matFoto).setIso(trimQuote(sol.get("element").asResource().getProperty(props.iso).asTriple().getMatchObject().toString()));
+				((MaquinaAventura)matFoto).setVelocidade(trimQuote(sol.get("element").asResource().getProperty(props.velo).asTriple().getMatchObject().toString()));
 			}
 			else if(type.equals("Reflex")) {
 				matFoto = new MaquinaReflex();
-				((MaquinaReflex)matFoto).setObjetiva(sol.get("element").asResource().getProperty(props.objIncl).asTriple().getMatchObject().toString());
-				((MaquinaReflex)matFoto).setNivelUser(sol.get("element").asResource().getProperty(props.nivUser).asTriple().getMatchObject().toString());
+				((MaquinaReflex)matFoto).setObjetiva(trimQuote(sol.get("element").asResource().getProperty(props.objIncl).asTriple().getMatchObject().toString()));
+				((MaquinaReflex)matFoto).setNivelUser(trimQuote(sol.get("element").asResource().getProperty(props.nivUser).asTriple().getMatchObject().toString()));
 
 			}
 
 			if(type.equals("Aventura") || type.equals("Reflex")) { //adulto
-				((MaquinaAdulto)matFoto).setFormVideo(sol.get("element").asResource().getProperty(props.formVid).asTriple().getMatchObject().toString());
-				((MaquinaAdulto)matFoto).setFotoSeq(sol.get("element").asResource().getProperty(props.fotoSeq).asTriple().getMatchObject().toString());
+				((MaquinaAdulto)matFoto).setFormVideo(trimQuote(sol.get("element").asResource().getProperty(props.formVid).asTriple().getMatchObject().toString()));
+				((MaquinaAdulto)matFoto).setFotoSeq(trimQuote(sol.get("element").asResource().getProperty(props.fotoSeq).asTriple().getMatchObject().toString()));
 			}
 
 			if(type.equals("Infatil")) {
 				matFoto = new MaquinaInfantil();
-				((MaquinaInfantil)matFoto).setCor(sol.get("element").asResource().getProperty(props.cor).asTriple().getMatchObject().toString());
+				((MaquinaInfantil)matFoto).setCor(trimQuote(sol.get("element").asResource().getProperty(props.cor).asTriple().getMatchObject().toString()));
 			}
 
 			if(type.equals("Aventura") || type.equals("Reflex") || type.equals("Infantil")) { //maquina
-				((Maquina)matFoto).setBateria(sol.get("element").asResource().getProperty(props.bateria).asTriple().getMatchObject().toString());
-				((Maquina)matFoto).setDimensao(sol.get("element").asResource().getProperty(props.dimensao).asTriple().getMatchObject().toString());
-				((Maquina)matFoto).setDimensaoEcra(sol.get("element").asResource().getProperty(props.dimEcra).asTriple().getMatchObject().toString());
-				((Maquina)matFoto).setDistFocal(sol.get("element").asResource().getProperty(props.distFocalMaq).asTriple().getMatchObject().toString());
-				((Maquina)matFoto).setInterf(sol.get("element").asResource().getProperty(props.interf).asTriple().getMatchObject().toString());
-				((Maquina)matFoto).setPeso(sol.get("element").asResource().getProperty(props.pesoCorpo).asTriple().getMatchObject().toString());
-				((Maquina)matFoto).setResolucao(sol.get("element").asResource().getProperty(props.resolucao).asTriple().getMatchObject().toString());
-				((Maquina)matFoto).setTipoMem(sol.get("element").asResource().getProperty(props.memoria).asTriple().getMatchObject().toString());
+				((Maquina)matFoto).setBateria(trimQuote(sol.get("element").asResource().getProperty(props.bateria).asTriple().getMatchObject().toString()));
+				((Maquina)matFoto).setDimensao(trimQuote(sol.get("element").asResource().getProperty(props.dimensao).asTriple().getMatchObject().toString()));
+				((Maquina)matFoto).setDimensaoEcra(trimQuote(sol.get("element").asResource().getProperty(props.dimEcra).asTriple().getMatchObject().toString()));
+				((Maquina)matFoto).setDistFocal(trimQuote(sol.get("element").asResource().getProperty(props.distFocalMaq).asTriple().getMatchObject().toString()));
+				((Maquina)matFoto).setInterf(trimQuote(sol.get("element").asResource().getProperty(props.interf).asTriple().getMatchObject().toString()));
+				((Maquina)matFoto).setPeso(trimQuote(sol.get("element").asResource().getProperty(props.pesoCorpo).asTriple().getMatchObject().toString()));
+				((Maquina)matFoto).setResolucao(trimQuote(sol.get("element").asResource().getProperty(props.resolucao).asTriple().getMatchObject().toString()));
+				((Maquina)matFoto).setTipoMem(trimQuote(sol.get("element").asResource().getProperty(props.memoria).asTriple().getMatchObject().toString()));
 
 			}
 
 			if(type.equals("Objetiva")) {
 				matFoto = new Objetiva();
-				((Objetiva)matFoto).setDistFoc(sol.get("element").asResource().getProperty(props.distFocal).asTriple().getMatchObject().toString());
-				((Objetiva)matFoto).setAbertura(sol.get("element").asResource().getProperty(props.abertura).asTriple().getMatchObject().toString());
-				((Objetiva)matFoto).setDimensoes(sol.get("element").asResource().getProperty(props.dimensoes).asTriple().getMatchObject().toString());
-				((Objetiva)matFoto).setPeso(sol.get("element").asResource().getProperty(props.peso).asTriple().getMatchObject().toString());
-				((Objetiva)matFoto).setTipo(sol.get("element").asResource().getProperty(props.tipo).asTriple().getMatchObject().toString());
+				((Objetiva)matFoto).setDistFoc(trimQuote(sol.get("element").asResource().getProperty(props.distFocal).asTriple().getMatchObject().toString()));
+				((Objetiva)matFoto).setAbertura(trimQuote(sol.get("element").asResource().getProperty(props.abertura).asTriple().getMatchObject().toString()));
+				((Objetiva)matFoto).setDimensoes(trimQuote(sol.get("element").asResource().getProperty(props.dimensoes).asTriple().getMatchObject().toString()));
+				((Objetiva)matFoto).setPeso(trimQuote(sol.get("element").asResource().getProperty(props.peso).asTriple().getMatchObject().toString()));
+				((Objetiva)matFoto).setTipo(trimQuote(sol.get("element").asResource().getProperty(props.tipo).asTriple().getMatchObject().toString()));
 			}
 
 			if(type.equals("Bolsas")) {
 				matFoto = new Bolsa();
-				((Bolsa)matFoto).setDimInterior(sol.get("element").asResource().getProperty(props.dimInt).asTriple().getMatchObject().toString());
-				((Bolsa)matFoto).setDimExterior(sol.get("element").asResource().getProperty(props.dimExt).asTriple().getMatchObject().toString());
+				((Bolsa)matFoto).setDimInterior(trimQuote(sol.get("element").asResource().getProperty(props.dimInt).asTriple().getMatchObject().toString()));
+				((Bolsa)matFoto).setDimExterior(trimQuote(sol.get("element").asResource().getProperty(props.dimExt).asTriple().getMatchObject().toString()));
 			}
 
-			matFoto.setTitulo(sol.get("element").asResource().getProperty(props.titulo).asTriple().getMatchObject().toString());
-			matFoto.setMarca(sol.get("element").asResource().getProperty(props.marca).asTriple().getMatchObject().toString());
-			matFoto.setPreco(sol.get("element").asResource().getProperty(props.preco).asTriple().getMatchObject().toString());
+			matFoto.setTitulo(trimQuote(sol.get("element").asResource().getProperty(props.titulo).asTriple().getMatchObject().toString()));
+			matFoto.setMarca(trimQuote(sol.get("element").asResource().getProperty(props.marca).asTriple().getMatchObject().toString()));
+			matFoto.setPreco(trimQuote(sol.get("element").asResource().getProperty(props.preco).asTriple().getMatchObject().toString()));
 
 			lista.add(matFoto);
 		}
@@ -178,4 +178,7 @@ public class Search {
 		return bolsa;
 	}
 
+	public String trimQuote(String str) {
+		return str.substring(1, str.length()-1);
+	}
 }
